53519f3e40233b1251e03711e2dd6876dd3132a7
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-15 15:20:52 +0530

    Added: Inventory Data Management

--- src/main/java/com/bdlz/oopsday11and12/InventoryData.java
+++ src/main/java/com/bdlz/oopsday11and12/InventoryData.java
@@ -70,4 +70,39 @@ public class InventoryData {
                 }
             }
         }
+
+    public static class Inventory {
+        String name;
+        double price;
+        double weight;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public double getPrice() {
+            return price;
+        }
+        public void setPrice(double price) {
+            this.price = price;
+        }
+        public double getWeight() {
+            return weight;
+        }
+        public void setWeight(double weight) {
+            this.weight = weight;
+        }
+
+        public Inventory(String name, double price, double weight) {
+
+            this.name = name;
+            this.price = price;
+            this.weight = weight;
+        }
+        public Inventory() {
+
+        }
+    }
 }
